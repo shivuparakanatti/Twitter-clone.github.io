4994db8a9f7851d569574aab735968dfbf1b7d44
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-06-15 23:52:11 +0530

    commit

---
+++ src/components/Aboute.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const About=()=>{
+    return(
+        <>
+        <h1 className="flex items-center justify-center font-bold text-4xl font-serif m-8 p-2">About US</h1>
+        <p className="flex items-center justify-center w-3/4 m-auto from-neutral-200 font-sans text-xl">Twitter is a social media platform that allows users to share short messages called tweets. It was launched in 2006 and has since become one of the most popular social media platforms in the world. With over 330 million active users, 
+            Twitter is used by people from all walks of life to share their thoughts and ideas with the world. Whether you’re a celebrity, politician, or just an ordinary person,
+             Twitter is a great way to connect with others and stay up-to-date on the latest news and trends.
+             </p>
+        
+        </>
+    )
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,43 @@
+'use client';
+
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+
+export default function Contact() {
+  return (
+    <form className="flex items-center justify-center my-12 flex-col gap-4">
+      <div>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="password1"
+            value="Your Name"
+          />
+        </div>
+        <TextInput
+          id="password1"
+          placeholder='Your name'
+          type="text"
+        />
+      </div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="email1"
+            value="Your email"
+          />
+        </div>
+        <TextInput
+          id="email1"
+          placeholder="Email ID"
+          required
+          type="email"
+        />
+      </div>
+    
+      <Button type="submit">
+        Contact me
+      </Button>
+    </form>
+  )
+}
+
+
