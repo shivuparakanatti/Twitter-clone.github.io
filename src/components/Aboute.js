832590e243999f32447ce3d31c4b37a604bd6977
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-11-08 22:40:30 +0530

    Update Aboute.js

--- src/components/Aboute.js
+++ src/components/Aboute.js
@@ -4,7 +4,7 @@ export const About=()=>{
     return(
         <>
 
-        
+    
         <h1 className="flex items-center justify-center font-bold text-4xl font-serif m-8 p-2">About US</h1>
         <p className="flex items-center justify-center w-3/4 m-auto from-neutral-200 font-sans text-xl">Twitter is a social media platform that allows users to share short messages called tweets. It was launched in 2006 and has since become one of the most popular social media platforms in the world. With over 330 million active users, 
             Twitter is used by people from all walks of life to share their thoughts and ideas with the world. Whether you’re a celebrity, politician, or just an ordinary person,
